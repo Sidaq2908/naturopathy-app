a2e575e40fe30f032724e7d141b8e2fd21aaf8cb
Sidaq <sidqu29801@gmail.com>
2025-07-08 17:04:16 +0530

    Add basename for react router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './i18n.js'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename='/naturopathy-app/'>
     <App />
     </BrowserRouter>
   
