b7cf8394f65abac2494e3d9fa128efcf4770fc7a
Sidaq <sidqu29801@gmail.com>
2025-07-14 12:40:29 +0530

    index.html main.jsx .env changed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './i18n.js'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename={process.env.NODE_ENV === 'production' ? "/naturopathy-app/" : undefined}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
     </BrowserRouter>
   
