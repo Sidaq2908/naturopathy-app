0da836c0ec232c8137e95b67a961298fd6a66d14
Sidaq <sidqu29801@gmail.com>
2025-07-15 16:11:43 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const isFtpDeploy = true;
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production'
-  ? (process.env.VERCEL ? '/' : '/naturopathy-app/')
-  : '/', 
+  base: isFtpDeploy
+    ? '/'
+    : process.env.NODE_ENV === 'production'
+    ? (process.env.VERCEL ? '/' : '/naturopathy-app/')
+    : '/',
 })
