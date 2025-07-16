30d94bc2b5811e758e9f35d1133238d052c82b1f
Sidaq <sidqu29801@gmail.com>
2025-07-16 17:05:58 +0530

    vite.config.js changed

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/'
-  // base: process.env.NODE_ENV === 'production'
-  // ? (process.env.VERCEL ? '/' : '/naturopathy-app/')
-  // : '/', 
+  base: process.env.NODE_ENV === 'production'
+  ? (process.env.VERCEL ? '/' : '/naturopathy-app/')
+  : '/', 
 })
